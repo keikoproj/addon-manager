300ed2e19c64469ffa7b62a0d00c27234b0c1c7d
Kevin D <kevdowney@gmail.com>
2019-10-09 13:26:56 -0700

    Fix tests

--- pkg/workflows/workflow_test.go
+++ pkg/workflows/workflow_test.go
@@ -147,6 +147,138 @@ spec:
           name: event-router-sa
           namespace: "{{workflow.parameters.namespace}}"
 `
+var wfArtifactsTemplate = `
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+spec:
+  entrypoint: entry
+  serviceAccountName: addon-manager-workflow-installer-sa
+  templates:
+  - name: entry
+    steps:
+    - - name: prereq-resources
+        template: submit
+        arguments:
+          artifacts:
+          - name: doc
+            path: /tmp/doc
+            raw:
+              data: |
+                apiVersion: v1
+                kind: Namespace
+                metadata:
+                  name: "{{workflow.parameters.namespace}}"
+                ---
+                apiVersion: v1
+                kind: ServiceAccount
+                metadata:
+                  labels:
+                    k8s-addon: cluster-autoscaler.addons.k8s.io
+                    k8s-app: cluster-autoscaler
+                  name: cluster-autoscaler
+                  namespace: "{{workflow.parameters.namespace}}"
+                ---
+                apiVersion: rbac.authorization.k8s.io/v1beta1
+                kind: ClusterRole
+                metadata:
+                  name: cluster-autoscaler
+                  labels:
+                    k8s-addon: cluster-autoscaler.addons.k8s.io
+                    k8s-app: cluster-autoscaler
+                rules:
+                - apiGroups: [""]
+                  resources: ["events","endpoints"]
+                  verbs: ["create", "patch"]
+                - apiGroups: [""]
+                  resources: ["pods/eviction"]
+                  verbs: ["create"]
+                - apiGroups: [""]
+                  resources: ["pods/status"]
+                  verbs: ["update"]
+                - apiGroups: [""]
+                  resources: ["endpoints"]
+                  resourceNames: ["cluster-autoscaler"]
+                  verbs: ["get","update"]
+                - apiGroups: [""]
+                  resources: ["nodes"]
+                  verbs: ["watch","list","get","update"]
+                - apiGroups: [""]
+                  resources: ["pods","services","replicationcontrollers","persistentvolumeclaims","persistentvolumes"]
+                  verbs: ["watch","list","get"]
+                - apiGroups: ["extensions"]
+                  resources: ["replicasets","daemonsets"]
+                  verbs: ["watch","list","get"]
+                - apiGroups: ["policy"]
+                  resources: ["poddisruptionbudgets"]
+                  verbs: ["watch","list"]
+                - apiGroups: ["apps"]
+                  resources: ["statefulsets", "replicasets"]
+                  verbs: ["watch","list","get"]
+                - apiGroups: ["storage.k8s.io"]
+                  resources: ["storageclasses"]
+                  verbs: ["watch","list","get"]
+                ---
+                apiVersion: rbac.authorization.k8s.io/v1
+                kind: Role
+                metadata:
+                  name: cluster-autoscaler
+                  namespace: "{{workflow.parameters.namespace}}"
+                  labels:
+                    k8s-addon: cluster-autoscaler.addons.k8s.io
+                    k8s-app: cluster-autoscaler
+                rules:
+                - apiGroups: [""]
+                  resources: ["configmaps"]
+                  verbs: ["create"]
+                - apiGroups: [""]
+                  resources: ["configmaps"]
+                  resourceNames: ["cluster-autoscaler-status"]
+                  verbs: ["delete","get","update"]
+                ---
+                apiVersion: rbac.authorization.k8s.io/v1
+                kind: ClusterRoleBinding
+                metadata:
+                  name: cluster-autoscaler
+                  labels:
+                    k8s-addon: cluster-autoscaler.addons.k8s.io
+                    k8s-app: cluster-autoscaler
+                roleRef:
+                  apiGroup: rbac.authorization.k8s.io
+                  kind: ClusterRole
+                  name: cluster-autoscaler
+                subjects:
+                  - kind: ServiceAccount
+                    name: cluster-autoscaler
+                    namespace: "{{workflow.parameters.namespace}}"
+
+                ---
+                apiVersion: rbac.authorization.k8s.io/v1
+                kind: RoleBinding
+                metadata:
+                  name: cluster-autoscaler
+                  namespace: "{{workflow.parameters.namespace}}"
+                  labels:
+                    k8s-addon: cluster-autoscaler.addons.k8s.io
+                    k8s-app: cluster-autoscaler
+                roleRef:
+                  apiGroup: rbac.authorization.k8s.io
+                  kind: Role
+                  name: cluster-autoscaler
+                subjects:
+                  - kind: ServiceAccount
+                    name: cluster-autoscaler
+                    namespace: "{{workflow.parameters.namespace}}"
+  - name: submit
+    inputs:
+      artifacts:
+        - name: doc
+          path: /tmp/doc
+    container:
+      image: expert360/kubectl-awscli:v1.11.2
+      command: [sh, -c]
+      args: ["kubectl apply -f /tmp/doc"]
+`
+
 func init() {
 	wf := &unstructured.Unstructured{}
 	wf.SetGroupVersionKind(schema.GroupVersionKind{
@@ -176,12 +308,12 @@ func TestNewWorkflowLifecycle(t *testing.T) {
 	g.Expect(wfl).To(BeAssignableToTypeOf(expected))
 }
 
-func TestWorkflowLifecycle_Install(t *testing.T) {
+func TestWorkflowLifecycle_Install_Resources(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	a := &v1alpha1.Addon{
+	addon := &v1alpha1.Addon{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "addon-wf",
+			Name:      "addon-wf-1",
 			Namespace: "default",
 		},
 		Spec: v1alpha1.AddonSpec{
@@ -199,7 +331,7 @@ func TestWorkflowLifecycle_Install(t *testing.T) {
 			},
 			Lifecycle: v1alpha1.LifecycleWorkflowSpec{
 				Prereqs: v1alpha1.WorkflowType{
-					Template:     wfPrereqsTemplate,
+					Template: wfPrereqsTemplate,
 				},
 				Install: v1alpha1.WorkflowType{
 					NamePrefix: "test",
@@ -210,24 +342,24 @@ func TestWorkflowLifecycle_Install(t *testing.T) {
 		},
 	}
 
-	wfl := NewWorkflowLifecycle(fclient, dynClient, a, rcdr, sch)
+	wfl := NewWorkflowLifecycle(fclient, dynClient, addon, rcdr, sch)
 	for _, lifecycle := range []v1alpha1.LifecycleStep{v1alpha1.Prereqs, v1alpha1.Install} {
 
-		wfName := a.GetFormattedWorkflowName(lifecycle)
-		wt, _ := a.GetWorkflowType(lifecycle)
+		wfName := addon.GetFormattedWorkflowName(lifecycle)
+		wt, _ := addon.GetWorkflowType(lifecycle)
 
 		phase, err := wfl.Install(context.Background(), wt, wfName)
 
 		g.Expect(err).To(Not(HaveOccurred()))
 		g.Expect(phase).To(Equal(v1alpha1.Pending))
 
-		var wfv1= &unstructured.Unstructured{}
+		var wfv1 = &unstructured.Unstructured{}
 		wfv1.SetGroupVersionKind(schema.GroupVersionKind{
 			Kind:    "Workflow",
 			Group:   "argoproj.io",
 			Version: "v1alpha1",
 		})
-		var wfv1Key= types.NamespacedName{Name: wfName, Namespace: "default"}
+		var wfv1Key = types.NamespacedName{Name: wfName, Namespace: "default"}
 		g.Eventually(func() error { return fclient.Get(context.TODO(), wfv1Key, wfv1) }, timeout).
 			Should(Succeed())
 
@@ -238,14 +370,103 @@ func TestWorkflowLifecycle_Install(t *testing.T) {
 		step := templates[1]
 		manifest, found, _ := unstructured.NestedString(step.(map[string]interface{}), "resource", "manifest")
 		g.Expect(found).To(BeTrue())
+
 		u := &unstructured.Unstructured{}
 		_ = yaml.Unmarshal([]byte(manifest), u)
 		labels := u.GetLabels()
-		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/name", "addon-wf"))
-		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/version", "1.0.0"))
-		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/part-of", "addon-wf"))
+		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/name", addon.GetName()))
+		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/version", addon.Spec.PkgVersion))
+		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/part-of", addon.GetName()))
 		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/managed-by", "addonmgr.keikoproj.io"))
 	}
+}
+
+func TestWorkflowLifecycle_Install_Artifacts(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	addon := &v1alpha1.Addon{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "addon-wf-2",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.AddonSpec{
+			PackageSpec: v1alpha1.PackageSpec{
+				PkgName:        "my-addon-2",
+				PkgVersion:     "1.2.0",
+				PkgType:        v1alpha1.HelmPkg,
+				PkgDescription: "",
+				PkgDeps:        map[string]string{"core/A": "*", "core/B": "v1.0.0"},
+			},
+			Selector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "my-app-2",
+				},
+			},
+			Lifecycle: v1alpha1.LifecycleWorkflowSpec{
+				Prereqs: v1alpha1.WorkflowType{
+					Template: wfArtifactsTemplate,
+				},
+				Install: v1alpha1.WorkflowType{
+					Template: wfArtifactsTemplate,
+				},
+			},
+		},
+	}
+
+	wfl := NewWorkflowLifecycle(fclient, dynClient, addon, rcdr, sch)
+	for _, lifecycle := range []v1alpha1.LifecycleStep{v1alpha1.Prereqs, v1alpha1.Install} {
+
+		wfName := addon.GetFormattedWorkflowName(lifecycle)
+		wt, _ := addon.GetWorkflowType(lifecycle)
+
+		phase, err := wfl.Install(context.Background(), wt, wfName)
+
+		g.Expect(err).To(Not(HaveOccurred()))
+		g.Expect(phase).To(Equal(v1alpha1.Pending))
+
+		var wfv1 = &unstructured.Unstructured{}
+		wfv1.SetGroupVersionKind(schema.GroupVersionKind{
+			Kind:    "Workflow",
+			Group:   "argoproj.io",
+			Version: "v1alpha1",
+		})
+		var wfv1Key = types.NamespacedName{Name: wfName, Namespace: "default"}
+		g.Eventually(func() error { return fclient.Get(context.TODO(), wfv1Key, wfv1) }, timeout).
+			Should(Succeed())
+
+		// Verify labels and annotations were added to resources
+		templates, found, _ := unstructured.NestedSlice(wfv1.UnstructuredContent(), "spec", "templates")
+		g.Expect(found).To(BeTrue())
+		template := templates[0]
+		var data string
+		if steps, found, _ := unstructured.NestedSlice(template.(map[string]interface{}), "steps"); found {
+			for _, step := range steps {
+				for _, stepTemplate := range step.([]interface{}) {
+					if artifacts, found, _ := unstructured.NestedFieldNoCopy(stepTemplate.(map[string]interface{}), "arguments", "artifacts"); found {
+						for _, argArtifact := range artifacts.([]interface{}) {
+							if manifest, found, _ := unstructured.NestedString(argArtifact.(map[string]interface{}), "raw", "data"); found {
+								data = manifest
+							} else {
+								t.Errorf("No raw data resources found. Expected that we would find resources at .spec.templates[].arguments.artifacts[].raw.data")
+							}
+						}
+					} else {
+						t.Errorf("No arguments artifacts found. Expected that we would find .spec.templates[].arguments.artifacts")
+					}
+				}
+			}
+		} else {
+			t.Errorf("No resources found. Expected that we would find one of resource patterns in workflow.")
+		}
+		u := &unstructured.Unstructured{}
+		_ = yaml.Unmarshal([]byte(data), u)
+		labels := u.GetLabels()
+		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/name", addon.GetName()))
+		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/version", addon.Spec.PkgVersion))
+		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/part-of", addon.GetName()))
+		g.Expect(labels).To(HaveKeyWithValue("app.kubernetes.io/managed-by", "addonmgr.keikoproj.io"))
+	}
+
 }
 
 // Test that an empty workflow type will fail
