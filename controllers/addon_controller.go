3f9392d706ec5c5b9f59cdaae5a6e007ed9df6ea
Ping Zou <ping_zou_2001@yahoo.com>
2019-10-11 12:01:06 -0700

    Add defer , recover to handle panics (#28)
    * Add defer , recover to handle runtime error

--- controllers/addon_controller.go
+++ controllers/addon_controller.go
@@ -123,9 +123,18 @@ func (r *AddonReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 		return reconcile.Result{}, ignoreNotFound(err)
 	}
+	
+	return r.execAddon(ctx, req, log, instance)
+}
 
+func (r *AddonReconciler) execAddon(ctx context.Context, req reconcile.Request, log logr.Logger, instance *addonmgrv1alpha1.Addon) (reconcile.Result, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Info("Error: Panic occurred during execAdd %s/%s due to %s", instance.Namespace, instance.Name, err)
+		}
+	}()
 	// Process addon instance
-	ret, err := r.processAddon(ctx, req, log, instance)
+	ret, procErr := r.processAddon(ctx, req, log, instance)
 
 	// Always update status, cache
 	err = r.updateAddonStatus(ctx, instance)
@@ -135,8 +144,7 @@ func (r *AddonReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ret, err
 	}
 	r.addAddonToCache(instance)
-
-	return ret, err
+	return ret, procErr
 }
 
 // SetupWithManager is called to setup manager and watchers
