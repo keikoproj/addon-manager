6f1150a389ab8264c5e2e1c38fbf21426e348602
jhu02 <jiminh@jiminhs-MacBook-Pro.local>
2022-02-16 14:48:59 -0800

    fix: update api
    Signed-off-by: jhu02 <jiminh@jiminhs-MacBook-Pro.local>

--- pkg/apis/addon/register.go
+++ pkg/apis/addon/register.go
@@ -1,1 +1,13 @@
 package addon
+
+// Addon constants
+const (
+	Group          string = "addonmgr.keikoproj.io"
+	Version        string = "v1alpha1"
+	APIVersion     string = Group + "/" + Version
+	AddonKind      string = "Addon"
+	AddonSingular  string = "addon"
+	AddonPlural    string = "addons"
+	AddonShortName string = "addon"
+	AddonFullName  string = AddonPlural + "." + Group
+)
