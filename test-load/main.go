d11fe916ee854a265ec89b74e43adb2a12830dcc
Kevin Downey <kevdowney@gmail.com>
2022-02-20 16:02:20 -0800

    Revert "Build Load test tool for addon manager (#103)"
    This reverts commit 187fc822203bc945149a1fc509c0593c7caa59ca.

--- test-load/main.go
+++ test-load/main.go
@@ -1,4 +1,18 @@
-package tests
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
 
 import (
 	"bufio"
@@ -6,80 +20,55 @@ import (
 	"context"
 	"fmt"
 	"io"
-
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/keikoproj/addon-manager/pkg/common"
 	"github.com/keikoproj/addon-manager/test-bdd/testutil"
-	"github.com/keikoproj/addon-manager/test-load/pkg/cmd"
-	"github.com/keikoproj/addon-manager/test-load/pkg/log"
-	"github.com/spf13/cobra"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
-)
 
-type flagpole struct {
-	managerPID         string // passed in kubeconfig
-	wfcontrollerPID    string
-	numberOfGoRoutines int
-}
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 const (
-	NumberOfWFPerRoutine = 200
-	eventNameBase        = 100
+	numberOfRoutines = 10
 )
 
-// NewCommand ...
-func NewCommand(logger log.Logger, streams cmd.IOStreams) *cobra.Command {
-	flags := &flagpole{}
-	cmd := &cobra.Command{
-		Args:  cobra.NoArgs,
-		Use:   "tests",
-		Short: "tests load",
-		Long:  "tests addon-manager loads",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return RunLoads(logger, flags)
-		},
-	}
-	cmd.Flags().StringVar(&flags.managerPID, "managerPID", "", " addon manager pid")
-	cmd.Flags().StringVar(&flags.wfcontrollerPID, "wfcontrollerPID", "", " workflow controller pid")
-	cmd.Flags().IntVar(&flags.numberOfGoRoutines, "numberOfGoRoutines", 10, " number of goroutines to be created. Each deploy 200 workflows.")
-	return cmd
-}
+func main() {
+	s, e := os.Getenv("LOADTEST_START_NUMBER"), os.Getenv("LOADTEST_END_NUMBER")
+	x, _ := strconv.Atoi(s)
+	y, _ := strconv.Atoi(e)
+	fmt.Printf("start = %d end = %d", x, y)
 
-func RunLoads(logger log.Logger, flags *flagpole) error {
-
-	if flags.numberOfGoRoutines == 0 {
-		fmt.Printf("please specify at least one goroutine.")
-		flags.numberOfGoRoutines = 2
-	}
-	perfsummary, err := os.Create("summary.txt")
+	costsummary, err := os.Create("summary.txt")
 	if err != nil {
-		return fmt.Errorf("failed creating summary file. %v", err)
+		return
 	}
-	dataWriter := bufio.NewWriter(perfsummary)
+	dataWriter := bufio.NewWriter(costsummary)
 
 	stop := make(chan bool)
+	mgrPid := os.Getenv("MANAGER_PID")
+	ctrlPid := os.Getenv("WFCTRL_PID")
 	go func(mgrPid, ctrlPid string, writer *bufio.Writer) {
 		for {
 			select {
 			case <-stop:
 				return
 			default:
-				fmt.Printf("\n every 2 minutes collecting mgr %s wfctrl %s %%cpu/%%mem ", mgrPid, ctrlPid)
+				fmt.Printf("\n every 2 minutes collecting data for mgr %s wfctrl %s", mgrPid, ctrlPid)
 				Summary(mgrPid, ctrlPid, writer)
 				time.Sleep(2 * time.Minute)
 			}
 		}
-	}(flags.managerPID, flags.wfcontrollerPID, dataWriter)
+	}(mgrPid, ctrlPid, dataWriter)
 
 	var wg sync.WaitGroup
-	wg.Add(flags.numberOfGoRoutines)
+	wg.Add(numberOfRoutines)
 	lock := &sync.Mutex{}
 
 	cfg := config.GetConfigOrDie()
@@ -90,22 +79,15 @@ func RunLoads(logger log.Logger, flags *flagpole) error {
 	var relativeAddonPath = "docs/examples/eventrouter.yaml"
 	var addonGroupSchema = common.AddonGVR()
 
-	for i := 1; i <= flags.numberOfGoRoutines; i++ {
+	for i := 1; i <= numberOfRoutines; i++ {
 		go func(i int, lock *sync.Mutex) {
 			defer wg.Done()
-			for j := i * eventNameBase; j < i*eventNameBase+int(NumberOfWFPerRoutine); j++ {
+			for j := i * 100; j < i*100+200; j++ {
 				addon, err := testutil.CreateLoadTestsAddon(lock, dynClient, relativeAddonPath, fmt.Sprintf("-%d", j))
 				if err != nil {
-					fmt.Printf("\n create addon failure for %v. retry 10 times after every 1 second. \n", err)
+					fmt.Printf("\n\n create addon failure err %v", err)
 					time.Sleep(1 * time.Second)
-					for i := 0; i < 10; i++ {
-						addon, err = testutil.CreateLoadTestsAddon(lock, dynClient, relativeAddonPath, fmt.Sprintf("-%d", j))
-						if err != nil || addon == nil {
-							time.Sleep(1 * time.Second)
-							continue
-						}
-						break
-					}
+					continue
 				}
 
 				addonName = addon.GetName()
@@ -113,7 +95,7 @@ func RunLoads(logger log.Logger, flags *flagpole) error {
 				for x := 0; x <= 500; x++ {
 					a, err := dynClient.Resource(addonGroupSchema).Namespace(addonNamespace).Get(ctx, addonName, metav1.GetOptions{})
 					if a == nil || err != nil || a.UnstructuredContent()["status"] == nil {
-						fmt.Printf("\n\n addon is not readdy err <%v> retry after 1 second ", err)
+						fmt.Printf("\n\n retry get addon status %v get ", err)
 						time.Sleep(1 * time.Second)
 						continue
 					}
@@ -124,11 +106,11 @@ func RunLoads(logger log.Logger, flags *flagpole) error {
 	}
 	wg.Wait()
 	stop <- true
-	perfsummary.Close()
-	return nil
+	costsummary.Close()
+
 }
 
-// capture cpu/memory/addons number periodically
+// capture cpu/memory/addons number every 3 mintues
 func Summary(managerPID, wfctrlPID string, datawriter *bufio.Writer) error {
 
 	kubectlCmd := exec.Command("kubectl", "-n", "addon-manager-system", "get", "addons")
@@ -162,8 +144,9 @@ func Summary(managerPID, wfctrlPID string, datawriter *bufio.Writer) error {
 	reader.Close()
 
 	AddonsNum := buf.String()
-	fmt.Printf("\n atm total addons number %s\n", AddonsNum)
+	fmt.Printf("\n addons number %s\n", AddonsNum)
 
+	//cmd = fmt.Sprintf("ps -p %s -o %%cpu,%%mem", managerPID)
 	cmd := exec.Command("ps", "-p", managerPID, "-o", "%cpu,%mem")
 	fmt.Printf("manager cpu cmd %v", *cmd)
 	managerUsage, err := cmd.Output()
@@ -171,6 +154,7 @@ func Summary(managerPID, wfctrlPID string, datawriter *bufio.Writer) error {
 		fmt.Printf("failed to collect addonmanager cpu/mem usage. %v", err)
 		return err
 	}
+	//fmt.Printf("\n addonmanager cpu/mem usage %s ", managerUsage)
 
 	cmd = exec.Command("ps", "-p", wfctrlPID, "-o", "%cpu,%mem")
 	wfControllerUsage, err := cmd.Output()
@@ -178,7 +162,7 @@ func Summary(managerPID, wfctrlPID string, datawriter *bufio.Writer) error {
 		fmt.Printf("failed to collect addonmanager cpu/mem usage. %v", err)
 		return err
 	}
-
+	//fmt.Printf("workflow controller cpu/mem usage %s ", wfControllerUsage)
 	fmt.Printf("addons number %s addonmanager cpu/mem usage %s controller cpu/mem usage %s", AddonsNum, managerUsage, wfControllerUsage)
 	oneline := fmt.Sprintf("<addons-num:%s  \n addon-mgr:%s  \nwf-controller:%s>", strings.TrimSpace(AddonsNum), strings.TrimSuffix(string(managerUsage), "\n"), strings.TrimSuffix(string(wfControllerUsage), "\n"))
 	datawriter.WriteString(oneline + "\n#############\n")
