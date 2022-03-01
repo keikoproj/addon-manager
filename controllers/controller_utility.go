f7bbdf6cc63e0211f37504b52f891cd3d6f95ebb
jhu02 <jiminh@jiminhs-MacBook-Pro.local>
2022-03-01 13:03:15 -0800

    Changes: remove un-used functionalities
    Signed-off-by: jhu02 <jiminh@jiminhs-MacBook-Pro.local>

--- controllers/controller_utility.go
+++ controllers/controller_utility.go
@@ -1,43 +1,23 @@
 package controllers
 
 import (
-	"context"
-	"fmt"
-	"reflect"
-	"sync"
-	"time"
-
 	"github.com/go-logr/logr"
 	"github.com/sirupsen/logrus"
 
-	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
-	informers "github.com/argoproj/argo-workflows/v3/pkg/client/informers/externalversions"
-	v1alpha1 "github.com/argoproj/argo-workflows/v3/pkg/client/informers/externalversions/workflow/v1alpha1"
-
 	"github.com/keikoproj/addon-manager/api/addon"
-	addonv1 "github.com/keikoproj/addon-manager/api/addon/v1alpha1"
 	addonv1versioned "github.com/keikoproj/addon-manager/pkg/client/clientset/versioned"
 	"github.com/keikoproj/addon-manager/pkg/client/clientset/versioned/scheme"
 	addonv1listers "github.com/keikoproj/addon-manager/pkg/client/listers/addon/v1alpha1"
-	"github.com/keikoproj/addon-manager/pkg/common"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/runtime"
-
-	// "k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/selection"
-	// "k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/dynamic"
-	// "k8s.io/client-go/informers/internalinterfaces"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	addonwfutility "github.com/keikoproj/addon-manager/pkg/workflows"
 
 	apiv1 "k8s.io/api/core/v1"
 	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -74,178 +54,10 @@ func NewWfInformers(nsInfo cache.SharedIndexInformer, stopCh <-chan struct{}, lo
 
 }
 
-func (wfinfo *WfInformers) Start(ctx context.Context) error {
-	cfg, _ := common.InClusterConfig()
-	addoncli := common.NewAddonClient(cfg)
-	if addoncli == nil {
-		return fmt.Errorf("failed to create addon client")
-	}
-	wfinfo.apiclientset = addoncli
-
-	wfcli := common.NewWFClient(cfg)
-	if wfcli == nil {
-		return fmt.Errorf("failed to create workflow client")
-	}
-	wfinformfactory := informers.NewSharedInformerFactory(wfcli, time.Second*30)
-	wfinfo.apiclientset = common.NewAddonClient(cfg)
-	wfinfo.nsInformers.AddEventHandler(
-		cache.ResourceEventHandlerFuncs{
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				wfinfo.handleWorkFlowUpdate(newObj, wfinformfactory.Argoproj().V1alpha1().Workflows())
-			},
-
-			AddFunc: func(obj interface{}) {
-				wfinfo.handleWorkFlowAdd(obj, wfinformfactory.Argoproj().V1alpha1().Workflows())
-			},
-		},
-	)
-
-	go wfinfo.nsInformers.Run(wfinfo.stopCh)
-	if ok := cache.WaitForCacheSync(wfinfo.stopCh, wfinfo.nsInformers.HasSynced); !ok {
-		return fmt.Errorf("failed to wait for caches to sync")
-	}
-
-	<-wfinfo.stopCh
-	return nil
-}
-
 func ResourcetweakListOptions() string {
 	req, _ := labels.NewRequirement(addon.ResourceDefaultManageByLabel, selection.Equals, []string{addon.ResourceDefaultManageByValue})
 	return labels.NewSelector().Add(*req).String()
 
-}
-
-// dedicated workflow add/update event handler
-func (wfinfo *WfInformers) handleWorkFlowUpdate(obj interface{}, informers v1alpha1.WorkflowInformer) {
-	if err := addonwfutility.IsValidV1WorkFlow(obj); err != nil {
-		fmt.Printf("not an expected workflow object %v", err)
-		return
-	}
-	wfobj := &wfv1.Workflow{}
-	_ = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.(*unstructured.Unstructured).UnstructuredContent(), wfobj)
-
-	// check the associated addons and update its status
-	msg := fmt.Sprintf("workflow %s/%s update status.phase %s", wfobj.GetNamespace(), wfobj.GetName(), wfobj.Status.Phase)
-	wfinfo.log.Info(msg)
-
-	if len(string(wfobj.Status.Phase)) == 0 {
-		wfinfo.log.Info("skip workflow ", wfobj.GetNamespace(), "/", wfobj.GetName(), "empty status update.")
-		return
-	}
-
-	// find the Addon from the namespace and update its status accordingly
-	addonName, lifecycle, err := addonwfutility.ExtractAddOnNameAndLifecycleStep(wfobj.GetName())
-	if err != nil {
-		return
-	}
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		wfinfo.updateAddonStatus(wfobj.GetNamespace(), addonName, lifecycle, wfobj.Status.Phase, wg)
-	}()
-}
-
-func (wfinfo *WfInformers) handleWorkFlowAdd(obj interface{}, informers v1alpha1.WorkflowInformer) {
-	if err := addonwfutility.IsValidV1WorkFlow(obj); err != nil {
-		fmt.Printf("not an expected workflow object %v", err)
-		return
-	}
-	wfobj := &wfv1.Workflow{}
-	_ = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.(*unstructured.Unstructured).UnstructuredContent(), wfobj)
-
-	// check the associated addons and update its status
-	msg := fmt.Sprintf("workflow %s/%s update status.phase %s", wfobj.GetNamespace(), wfobj.GetName(), wfobj.Status.Phase)
-	wfinfo.log.Info(msg)
-	if len(string(wfobj.Status.Phase)) == 0 {
-		msg := fmt.Sprintf("skip %s/%s workflow empty status.", wfobj.GetNamespace(),
-			wfobj.GetName())
-		wfinfo.log.Info(msg)
-		return
-	}
-	addonName, lifecycle, err := addonwfutility.ExtractAddOnNameAndLifecycleStep(wfobj.GetName())
-	if err != nil {
-		return
-	}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		wfinfo.updateAddonStatus(wfobj.GetNamespace(), addonName, lifecycle, wfobj.Status.Phase, wg)
-	}()
-}
-
-func (wfinfo *WfInformers) updateAddonStatus(namespace, name, lifecycle string, lifecyclestatus wfv1.WorkflowPhase, wg *sync.WaitGroup) error {
-	defer wg.Done()
-
-	msg := fmt.Sprintf("updating addon %s/%s step %s status to %s\n", namespace, name, lifecycle, lifecyclestatus)
-	wfinfo.log.Info(msg)
-
-	addonobj, err := wfinfo.addonlister.Addons(namespace).Get(name)
-	if err != nil || addonobj == nil {
-		msg := fmt.Sprintf("failed getting addon %s/%s, err %v", namespace, name, err)
-		fmt.Print(msg)
-		return fmt.Errorf(msg)
-
-	}
-
-	updating := addonobj.DeepCopy()
-	prevStatus := addonobj.Status
-	cycle := prevStatus.Lifecycle
-	msg = fmt.Sprintf("addon %s/%s cycle %v", updating.Namespace, updating.Name, cycle)
-	wfinfo.log.Info(msg)
-
-	newStatus := addonv1.AddonStatus{
-		Lifecycle: addonv1.AddonStatusLifecycle{},
-		Resources: []addonv1.ObjectStatus{},
-	}
-	if lifecycle == "prereqs" {
-		newStatus.Lifecycle.Prereqs = addonv1.ApplicationAssemblyPhase(lifecyclestatus)
-		newStatus.Lifecycle.Installed = prevStatus.Lifecycle.Installed
-	} else if lifecycle == "install" || lifecycle == "delete" {
-		newStatus.Lifecycle.Installed = addonv1.ApplicationAssemblyPhase(lifecyclestatus)
-		newStatus.Lifecycle.Prereqs = prevStatus.Lifecycle.Prereqs
-	}
-	newStatus.Resources = append(newStatus.Resources, prevStatus.Resources...)
-	newStatus.Checksum = prevStatus.Checksum
-	newStatus.Reason = prevStatus.Reason
-	newStatus.StartTime = prevStatus.StartTime
-
-	if reflect.DeepEqual(newStatus, prevStatus) {
-		msg := fmt.Sprintf("addon %s/%s lifecycle the same. skip update.", updating.Namespace, updating.Name)
-		wfinfo.log.Info(msg)
-		return nil
-	}
-	updating.Status = newStatus
-	updated, err := wfinfo.apiclientset.AddonmgrV1alpha1().Addons(updating.Namespace).UpdateStatus(context.Background(), updating,
-		metav1.UpdateOptions{})
-	if err != nil {
-		msg := fmt.Sprintf("Addon %s/%s status could not be updated. %v", updating.Namespace, updating.Name, err)
-		fmt.Print(msg)
-	}
-	cycle = updated.Status.Lifecycle
-	msg = fmt.Sprintf("addon %s/%s updated cycle %v", updating.Namespace, updating.Namespace, cycle)
-	wfinfo.log.Info(msg)
-
-	if lifecycle == "delete" && updating.Status.Lifecycle.Installed.Completed() {
-		wfinfo.delAddon(updating.Namespace, updating.Namespace)
-	}
-
-	msg = fmt.Sprintf("successfully update addon %s/%s step %s status to %s", namespace, name, lifecycle, lifecyclestatus)
-	wfinfo.log.Info(msg)
-	return nil
-}
-
-func (wfinfo *WfInformers) delAddon(namespace, name string) error {
-	delseconds := int64(0)
-	err := wfinfo.apiclientset.AddonmgrV1alpha1().Addons(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{GracePeriodSeconds: &delseconds})
-	if err != nil {
-		msg := fmt.Sprintf("failed deleting addon %s/%s, err %v", namespace, name, err)
-		fmt.Print(msg)
-		return fmt.Errorf(msg)
-	}
-	msg := fmt.Sprintf("\n Successfully deleting addon %s/%s\n", namespace, name)
-	fmt.Print(msg)
-	return nil
 }
 
 const defaultSpamBurst = 10000
