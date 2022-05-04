96b2da259fd36cddce0f91b6795df5ebfc64b694
jhu02 <jiminh@jiminhs-MacBook-Pro.local>
2022-05-04 15:46:23 -0700

    cleanup
    Signed-off-by: jhu02 <jiminh@jiminhs-MacBook-Pro.local>

--- api/addon/register.go
+++ api/addon/register.go
@@ -39,8 +39,4 @@ const (
 	ResourceDefaultPartLabel     = "app.kubernetes.io/part-of"
 
 	TTL = time.Duration(1) * time.Hour // 1 hour
-
-	// complete status - addon either succeed, failed, or error
-	AddonCompleteLabel   = "addons.addonmgr.keikoproj.io/completed"
-	AddonCompleteTrueKey = "true"
 )
