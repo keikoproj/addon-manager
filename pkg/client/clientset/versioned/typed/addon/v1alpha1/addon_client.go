a09b939b6a8da8d2acc6437d3a349360ad9530c2
Todd Ekenstam <Todd_Ekenstam@intuit.com>
2023-09-15 20:30:50 -0700

    Update Kubernetes runtime and build tools
    Bump controller-runtime to v0.13.1
    Bump controller-tools to v0.12.1
    Bump code-generator to v0.25.12
    Bump Kind to 1.25
    Regenerate controller code
    
    Signed-off-by: Todd Ekenstam <Todd_Ekenstam@intuit.com>

--- pkg/client/clientset/versioned/typed/addon/v1alpha1/addon_client.go
+++ pkg/client/clientset/versioned/typed/addon/v1alpha1/addon_client.go
@@ -3,6 +3,8 @@
 package v1alpha1
 
 import (
+	"net/http"
+
 	v1alpha1 "github.com/keikoproj/addon-manager/api/addon/v1alpha1"
 	"github.com/keikoproj/addon-manager/pkg/client/clientset/versioned/scheme"
 	rest "k8s.io/client-go/rest"
@@ -23,12 +25,28 @@ func (c *AddonmgrV1alpha1Client) Addons(namespace string) AddonInterface {
 }
 
 // NewForConfig creates a new AddonmgrV1alpha1Client for the given config.
+// NewForConfig is equivalent to NewForConfigAndClient(c, httpClient),
+// where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*AddonmgrV1alpha1Client, error) {
 	config := *c
 	if err := setConfigDefaults(&config); err != nil {
 		return nil, err
 	}
-	client, err := rest.RESTClientFor(&config)
+	httpClient, err := rest.HTTPClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+	return NewForConfigAndClient(&config, httpClient)
+}
+
+// NewForConfigAndClient creates a new AddonmgrV1alpha1Client for the given config and http client.
+// Note the http client provided takes precedence over the configured transport values.
+func NewForConfigAndClient(c *rest.Config, h *http.Client) (*AddonmgrV1alpha1Client, error) {
+	config := *c
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
+	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
 	}
